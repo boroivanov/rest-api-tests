c4cbd5211f8ad0e3c08a447112346df59ee9ab5d
Borislav Ivanov <borogl@gmail.com>
2018-08-04 10:58:36 -0700

    Add base test

--- tests/system/test_home.py
+++ tests/system/test_home.py
@@ -1,11 +1,10 @@
-from unittest import TestCase
-from app import app
+from tests.system.base_test import BaseTest
 import json
 
 
-class TestHome(TestCase):
+class TestHome(BaseTest):
     def test_home(self):
-        with app.test_client() as c:
+        with self.app as c:
             r = c.get('/')
 
         self.assertEqual(r.status_code, 200)
