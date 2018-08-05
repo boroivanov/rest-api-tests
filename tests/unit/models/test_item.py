7d14ea312fc2a54d2949e9dbe5620a8742dcad0f
Borislav Ivanov <borogl@gmail.com>
2018-08-05 10:20:20 -0700

    Add ItemModel unit tests

---
+++ tests/unit/models/test_item.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from models.item import ItemModel
+
+
+class TestItemModel(TestCase):
+    def test_init(self):
+        item = ItemModel('test', 123.00)
+        self.assertEqual(item.name, 'test')
+        self.assertEqual(item.price, 123.00)
+
+    def test_json(self):
+        item = ItemModel('test', 123.00)
+        self.assertDictEqual(item.json(), {'name': 'test', 'price': 123.00})
