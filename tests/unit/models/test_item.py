59f44035d7aa425779d3dcdab5c45619d2bba79b
Borislav Ivanov <borogl@gmail.com>
2018-08-11 10:30:17 -0700

    Check store id in unit tests

--- tests/unit/models/test_item.py
+++ tests/unit/models/test_item.py
@@ -7,7 +7,9 @@ class TestItemModel(BaseTest):
         item = ItemModel('test', 123.00, 1)
         self.assertEqual(item.name, 'test')
         self.assertEqual(item.price, 123.00)
+        self.assertEqual(item.store_id, 1)
 
     def test_json(self):
         item = ItemModel('test', 123.00, 1)
         self.assertDictEqual(item.json(), {'name': 'test', 'price': 123.00})
+        self.assertEqual(item.store_id, 1)
