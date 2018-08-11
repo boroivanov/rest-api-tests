422c86d08b34eeed47fb615d0faf58460eb858ea
Borislav Ivanov <borogl@gmail.com>
2018-08-11 09:55:01 -0700

    Include store id in item integration test

--- tests/integration/models/test_item.py
+++ tests/integration/models/test_item.py
@@ -1,11 +1,13 @@
 from tests.base_test import BaseTest
 from models.item import ItemModel
+from models.store import StoreModel
 
 
 class TestItem(BaseTest):
     def test_crud(self):
         with self.app_context():
-            item = ItemModel('test', 123.00)
+            StoreModel('test').save_to_db()
+            item = ItemModel('test', 123.00, 1)
             self.assertIsNone(ItemModel.find_by_name('test'),
                               f'Found an item with name ' +
                               '{item.name}, but expected not to.')
--- tests/unit/models/test_item.py
+++ tests/unit/models/test_item.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
-
 from models.item import ItemModel
 
 
 class TestItemModel(TestCase):
     def test_init(self):
-        item = ItemModel('test', 123.00)
+        item = ItemModel('test', 123.00, 1)
         self.assertEqual(item.name, 'test')
         self.assertEqual(item.price, 123.00)
 
     def test_json(self):
-        item = ItemModel('test', 123.00)
+        item = ItemModel('test', 123.00, 1)
         self.assertDictEqual(item.json(), {'name': 'test', 'price': 123.00})
