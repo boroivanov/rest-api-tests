261b400107d69dce79a07fc6f6aaafb305c4b63d
Borislav Ivanov <borogl@gmail.com>
2018-08-11 10:48:03 -0700

    Add store unit test

---
+++ tests/unit/models/test_store.py
@@ -0,0 +1,10 @@
+from models.store import StoreModel
+from tests.unit.base_test import BaseTest
+
+
+class TestStoreModel(BaseTest):
+    def test_create_store(self):
+        store = StoreModel('test')
+        self.assertEqual(store.name, 'test',
+                         'The name of the store does not equal ' +
+                         'the constuctor argument')
